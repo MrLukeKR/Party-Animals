f647644645eab5305e21ccf3a31c57fe846712b9
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2018-02-19 18:44:48 +0000

    Initial commit

---
+++ G53GRA.Framework/Code/MyScene.cpp
@@ -0,0 +1,19 @@
+#include "MyScene.h"
+
+MyScene::MyScene(int argc, char** argv, const char *title, const int& windowWidth, const int& windowHeight)
+	: Scene(argc, argv, title, windowWidth, windowHeight)
+{
+
+}
+
+void MyScene::Initialise()
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+}
+
+void MyScene::Projection()
+{
+	GLdouble aspect = static_cast<GLdouble>(windowWidth) / static_cast<GLdouble>(windowHeight);
+	gluPerspective(60.0, aspect, 1.0, 1000.0);
+}
---
+++ G53GRA.Framework/Code/MyScene.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "../Framework/Engine/Scene.h"
+
+class MyScene :
+	public Scene
+{
+public:
+	MyScene(int argc, char** argv, const char *title, const int& windowWidth, const int& windowHeight);
+	~MyScene() {};
+
+private:
+	void Initialise();
+	void Projection();
+
+};
