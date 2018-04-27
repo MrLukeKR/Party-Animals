dbff3074ade3c1cda73e60ce3badb9671a77c365
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2018-04-27 07:41:51 +0100

    Refactored and commented the Moon

--- G53GRA.Framework/Code/Objects/Lights/Moon.cpp
+++ G53GRA.Framework/Code/Objects/Lights/Moon.cpp
@@ -1,54 +1,48 @@
 #include "Moon.h"
 
-Moon::Moon()
-{
-}
+/*
+Party Animals: Moon
+Author: Luke K. Rose
+April 2018
+*/
 
-Moon::~Moon()
-{
-}
-
+/*
+Draws the object
+*/
 void Moon::Display() {
 	glPushMatrix();
 		glPushAttrib(GL_ALL_ATTRIB_BITS);
+			glRotatef(orbitRotation, 0, 0, 1); //Rotate the moon about its orbit
 			glTranslatef(pos[0], pos[1], pos[2]);
 			glScalef(scale[0], scale[1], scale[2]);
 			glRotatef(rotation[1], 0.f, 1.f, 0.f);
 			glRotatef(rotation[2], 0.f, 0.f, 1.f);
 			glRotatef(rotation[0], 1.f, 0.f, 0.f);
 	
-			glPushMatrix();
-				glTranslatef(-pos[0], -pos[1], -pos[2]);
-				glRotatef(orbitRotation, 0, 0, 1);
-				glTranslatef(pos[0], pos[1], pos[2]);
-				glColor3f(1.f,1.f,1.f);
-	
-				glutSolidSphere(scale[0],10,10);	
-
-			glPopMatrix();
+			glColor4f(1.f,1.f,1.f, 1.f); //Set the colour to opaque white
+			glutSolidSphere(scale[0], 10, 10); //Draw the moon
 		glPopAttrib();
 	glPopMatrix();
-}
-
-void Moon::Update(const double& deltaTime)
-{
-	if((int) orbitRotation == 0)
-		glEnable(GL_LIGHT2);
-	else if((int)orbitRotation == 180)
-		glDisable(GL_LIGHT2);
-
-	for (int i = 0; i < 4; i++) {
-		//light_ambient[i] = sin(orbitRotation * M_PI / 180) / 100.f;
-		light_diffuse[i] = sin(orbitRotation * M_PI / 180);
-	}
 
 	glLightfv(GL_LIGHT2, GL_DIFFUSE, light_diffuse);
 	glLightfv(GL_LIGHT2, GL_SPECULAR, light_diffuse);
 	glLightfv(GL_LIGHT2, GL_POSITION, light_position);
+}
 
-	//printf("%f - %f\r\n",orbitRotation, sin(orbitRotation * M_PI / 180 ));
+/*
+Updates animation angles and translation vectors for the object
+*/
+void Moon::Update(const double& deltaTime)
+{
+	if((int) orbitRotation == 0) //Turn on the moon if it's above the world
+		glEnable(GL_LIGHT2);
+	else if((int)orbitRotation == 180) //Turn off the moon if it's below the world
+		glDisable(GL_LIGHT2);
 
-	orbitRotation += .1f;
-	if (orbitRotation >= 360.f)
+	for (int i = 0; i < 4; i++) //Set the brightness of the moon
+		light_diffuse[i] = (float)sin(orbitRotation * M_PI / 180.f);
+
+	orbitRotation += .1f; //Update the orbit of the moon
+	if (orbitRotation >= 360.f) //Reset the orbit of the moon if it exceeds the bounds
 		orbitRotation = 0;
 }
