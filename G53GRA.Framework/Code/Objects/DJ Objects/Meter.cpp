bb2faf992d6a36e6222ba0f3997f833d2b9c999d
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2018-04-27 06:51:17 +0100

    Refactored and commented on the Meter

--- G53GRA.Framework/Code/Objects/DJ Objects/Meter.cpp
+++ G53GRA.Framework/Code/Objects/DJ Objects/Meter.cpp
@@ -1,6 +1,15 @@
 #include "Meter.h"
 #include "Objects\Box.h"
 
+/*
+Party Animals: Meter
+Author: Luke K. Rose
+April 2018
+*/
+
+/*
+Draws the object
+*/
 void Meter::Display() {
 	glPushMatrix();
 		glPushAttrib(GL_ALL_ATTRIB_BITS);
@@ -12,39 +21,34 @@ void Meter::Display() {
 			glRotatef(rotation[2], 0.f, 0.f, 1.f);
 		
 			glDisable(GL_LIGHTING);
-			glColor4f(1, 1, 1, 1);
-	
-			glEnable(GL_TEXTURE_2D);
-			glEnable(GL_COLOR_MATERIAL);
-		
-			glBindTexture(GL_TEXTURE_2D, meterTex);
-		
-			glBegin(GL_QUADS);
-				glTexCoord2f(0 + offset[0], 0 + offset[1]);
-				glVertex3f(0, 0, 0);
-		
-				glTexCoord2f(offset[0]+ 0.24f, 0 + offset[1]);
-				glVertex3f(1, 0, 0);
-		
-				glTexCoord2f(offset[0] + 0.24f, offset[1] + 0.24f);
-				glVertex3f(1, 1, 0);
-		
-				glTexCoord2f(0 + offset[0], offset[1] + 0.24f);
-				glVertex3f(0, 1, 0);
-			glEnd();
-
-			glDisable(GL_COLOR_MATERIAL);
-			glBindTexture(GL_TEXTURE_2D, NULL);
-			glDisable(GL_TEXTURE_2D);
-			
+				glColor4f(1, 1, 1, 1); //Set the colour to opaque white
+				glEnable(GL_TEXTURE_2D);
+					glEnable(GL_COLOR_MATERIAL);
+						glBindTexture(GL_TEXTURE_2D, meterTex);
+							glBegin(GL_QUADS); //Draw a plane and calculate the subsection of the spritesheet to use at this point
+								glTexCoord2f(0 + offset[0], 0 + offset[1]);
+									glVertex3f(0, 0, 0);
+								glTexCoord2f(offset[0]+ 0.24f, 0 + offset[1]);
+									glVertex3f(1, 0, 0);
+								glTexCoord2f(offset[0] + 0.24f, offset[1] + 0.24f);
+									glVertex3f(1, 1, 0);
+								glTexCoord2f(0 + offset[0], offset[1] + 0.24f);
+									glVertex3f(0, 1, 0);
+							glEnd();
+						glDisable(GL_COLOR_MATERIAL);
+					glBindTexture(GL_TEXTURE_2D, NULL);
+				glDisable(GL_TEXTURE_2D);
 			glEnable(GL_LIGHTING);
 		glPopAttrib();
 	glPopMatrix();
 }
 
+/*
+Updates animation angles and translation vectors for the object
+*/
 void Meter::Update(const double& deltaTime) {
 	if ((time += deltaTime) >= .1f) {
-		offset[0] += 0.25f;
+		offset[0] += 0.25f; //Increment the texture coordinate offset by 0.25 (The file has 4 x 4 images and has a total size of 1.0 x 1.0)
 		time = 0;
 	}
 
@@ -54,6 +58,5 @@ void Meter::Update(const double& deltaTime) {
 		offset[1] += 0.25f;
 	}
 
-	if (offset[1] >= 1)
-		offset[1] = 0;
+	if (offset[1] >= 1) offset[1] = 0;
 }
--- G53GRA.Framework/Code/Objects/DJ Objects/Meter.h
+++ G53GRA.Framework/Code/Objects/DJ Objects/Meter.h
@@ -1,20 +1,36 @@
 #pragma once
+
 #include "DisplayableObject.h"
 #include "Animation.h"
+
+/*
+Party Animals: Meter (Header File)
+Author: Luke K. Rose
+April 2018
+*/
 
 class Meter :
 	public DisplayableObject,
 	public Animation
 {
 public:
+	/*
+	Public Constructor/Destructor
+	*/
 	Meter() {};
 	~Meter() {};
 
+	/*
+	Public methods
+	*/
 	void Display();
 	void Update(const double& dT);
-private:
-	GLuint meterTex = Scene::GetTexture("./Textures/meter.bmp");
 
+private:
+	/*
+	Private variables
+	*/
+	GLuint meterTex = Scene::GetTexture("./Textures/meter.bmp");
 	float offset[2] = { 0,0 };
 	double time = 0;
 };
