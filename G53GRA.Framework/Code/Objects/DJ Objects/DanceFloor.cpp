3b43c5ee51ce88dd4ad3ced228eb2decbca62715
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2018-04-27 06:39:00 +0100

    Refactored and commented the Dance Floor

--- G53GRA.Framework/Code/Objects/DJ Objects/DanceFloor.cpp
+++ G53GRA.Framework/Code/Objects/DJ Objects/DanceFloor.cpp
@@ -1,50 +1,46 @@
 #include "DanceFloor.h"
 
+/* 
+Party Animals: Dance Floor
+Author: Luke K. Rose
+April 2018
+*/
 
-
-DanceFloor::DanceFloor()
-{
-}
-
-
-DanceFloor::~DanceFloor()
-{
-}
-
+/*
+Draws the object
+*/
 void DanceFloor::Display() {
 	glPushMatrix();
-	glPushAttrib(GL_ALL_ATTRIB_BITS);
+		glPushAttrib(GL_ALL_ATTRIB_BITS);
 
-	glTranslatef(pos[0], pos[1], pos[2]);
-	glScalef(scale[0], scale[1], scale[2]);
-	glRotatef(rotation[1], 0.f, 1.f, 0.f);
-	glRotatef(rotation[0], 1.f, 0.f, 0.f);
-	glRotatef(rotation[2], 0.f, 0.f, 1.f);
+			glTranslatef(pos[0], pos[1], pos[2]);
+			glScalef(scale[0], scale[1], scale[2]);
+			glRotatef(rotation[1], 0.f, 1.f, 0.f);
+			glRotatef(rotation[0], 1.f, 0.f, 0.f);
+			glRotatef(rotation[2], 0.f, 0.f, 1.f);
 
-	if(animTime++ > 1000) animTime = 0;
-
-	for (int x = -10; x < 10; x++)
-	{
-		for (int y = -10; y < 10; y++)
-		{
-			int loc = (abs(x) + abs(y) + animTime / 50) % 7;
-
-			glColor3f(red[loc],green[loc],blue[loc]);
+			for (float x = -10; x < 10; x++) //Draw a 20 x 20 multicoloured dance floor
+				for (float y = -10; y < 10; y++)
+				{
+					int loc = (int)(abs(x) + abs(y) + animTime / 50) % 7; //Determine which colour to use on this square
 		
-			glBegin(GL_QUADS);
-			glNormal3f(0, 1, 0);
-			glTexCoord2d(0, 0);
-			glVertex3f(x, -0.1f, y);
-			glTexCoord2d(1, 0);
-			glVertex3f(x, -0.1f, y + 1);
-			glTexCoord2d(1, 1);
-			glVertex3f(x + 1, -0.1f, y + 1);
-			glTexCoord2d(0, 1);
-			glVertex3f(x + 1, -0.1f, y);
-			glEnd();
-		}
-	}
-
-	glPopAttrib();
+					glColor4f(red[loc],green[loc],blue[loc], 1.f); // Set the colour of this square
+				
+					glBegin(GL_QUADS); //Draw the square
+						glVertex3f(x, -0.1f, y);
+						glVertex3f(x, -0.1f, y + 1);
+						glVertex3f(x + 1, -0.1f, y + 1);
+						glVertex3f(x + 1, -0.1f, y);
+					glEnd();
+				}
+		glPopAttrib();
 	glPopMatrix();
+}
+
+/*
+Updates animation angles and translation vectors for the object
+*/
+void DanceFloor::Update(double const& dT) {
+	if (animTime++ > 1000)  //Increment the animation time
+		animTime = 0; //Reset the animation time if it exceeds the bounds
 }
--- G53GRA.Framework/Code/Objects/DJ Objects/DanceFloor.h
+++ G53GRA.Framework/Code/Objects/DJ Objects/DanceFloor.h
@@ -1,17 +1,38 @@
 #pragma once
+
 #include "DisplayableObject.h"
+#include "Animation.h"
+
+/*
+Party Animals: Dance Floor (Header File)
+Author: Luke K. Rose
+April 2018
+*/
+
 class DanceFloor :
-	public DisplayableObject
+	public DisplayableObject,
+	public Animation
 {
 public:
-	DanceFloor();
-	~DanceFloor();
+	/*
+	Public Constructor/Destructor
+	*/
+	DanceFloor() {};
+	~DanceFloor() {};
 
+	/*
+	Public methods
+	*/
 	void Display();
+	void Update(const double& dT);
+
 private:
+	/*
+	Private variables
+	*/
 	int animTime = 0;
-	float red[6] = {0,0,0,1,1,1};
-	float green[6] = {0,1,1,0,0,1};
-	float blue[6] = {1,0,1,0,1,0};
+	float red[6]   = {0,0,0,1,1,1},
+		  green[6] = {0,1,1,0,0,1},
+		  blue[6]  = {1,0,1,0,1,0};
 
 };
