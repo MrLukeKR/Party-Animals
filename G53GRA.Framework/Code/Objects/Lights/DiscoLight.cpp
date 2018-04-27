b8ab96d9a05a992789491ff0cdae3600bdd67d7d
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2018-04-27 07:38:10 +0100

    Refactored and commented the Disco Lights

--- G53GRA.Framework/Code/Objects/Lights/DiscoLight.cpp
+++ G53GRA.Framework/Code/Objects/Lights/DiscoLight.cpp
@@ -1,31 +1,37 @@
 #include "DiscoLight.h"
 #include "Objects\Box.h"
 
+/*
+Party Animals: Disco Light
+Author: Luke K. Rose
+April 2018
+*/
+
+/*
+Initialise the light source by enabling it and setting its direction and colour
+*/
 DiscoLight::DiscoLight(GLfloat dirX, GLfloat dirY, GLfloat dirZ, GLenum light, float colorSeed) : lightSource(light), light_direction{ dirX, dirY, dirZ }, value(colorSeed)
 {
 	glEnable(lightSource);
 }
 
-
-DiscoLight::~DiscoLight()
-{
-}
-
+/*
+Draws the object
+*/
 void DiscoLight::Display() {
 	glPushMatrix();
-	glPushAttrib(GL_ALL_ATTRIB_BITS);
-
-	glTranslatef(pos[0], pos[1], pos[2]);
-	glScalef(scale[0], scale[1], scale[2]);
-	glRotatef(rotation[0], 1.f, 0.f, 0.f);
-	glRotatef(rotation[1], 0.f, 1.f, 0.f);
-	glRotatef(rotation[2], 0.f, 0.f, 1.f);
-	glColor3f(light_ambient[0], light_ambient[1], light_ambient[2]);
-	Box::box(1,1,1);
-
-	glPopAttrib();
+		glPushAttrib(GL_ALL_ATTRIB_BITS);
+			glTranslatef(pos[0], pos[1], pos[2]);
+			glScalef(scale[0], scale[1], scale[2]);
+			glRotatef(rotation[0], 1.f, 0.f, 0.f);
+			glRotatef(rotation[1], 0.f, 1.f, 0.f);
+			glRotatef(rotation[2], 0.f, 0.f, 1.f);
+			glColor4f(light_ambient[0], light_ambient[1], light_ambient[2], 1); //Set the colour of the light
+			Box::box(1, 1, 1); //Draw a 1 x 1 x 1 cube
+		glPopAttrib();
 	glPopMatrix();
 
+	//Local lighting setup:
 	glLightfv(lightSource, GL_AMBIENT, light_ambient);
 	glLightfv(lightSource, GL_DIFFUSE, light_diffuse);
 	glLightfv(lightSource, GL_SPECULAR, light_specular);
@@ -35,18 +41,16 @@ void DiscoLight::Display() {
 	glLightfv(lightSource, GL_POSITION, light_position);
 }
 
+/*
+Updates animation angles and translation vectors for the object
+*/
 void DiscoLight::Update(const double& deltaTime) {
-	light_position[0] = pos[0];
+	light_position[0] = pos[0]; //Animate the light's position
 	light_position[1] = pos[1];
 	light_position[2] = pos[2];
 
-	static bool ambShift = true;
+	for (int i = 0; i < 3; i++) 
+		light_ambient[i] = (float)(((int)(value += 0.075f) >> i) & 1); //Set the colour of the light
 
-	for (int i = 0; i < 3; i++) {
-		light_ambient[i] = ((int)value >> i) & 1;
-	}
-
-	value += 0.075f;
-	if (value == 7)
-		value = 1;
+	if (value == 7) value = 1; //Reset the colour wheel back to the beginning
 }
