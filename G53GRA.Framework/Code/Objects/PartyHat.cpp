1e46084bfcc1bd85e0d839fc78213749ed7e2c54
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2018-04-27 07:53:49 +0100

    Refactored and commented Party Hat

--- G53GRA.Framework/Code/Objects/PartyHat.cpp
+++ G53GRA.Framework/Code/Objects/PartyHat.cpp
@@ -19,21 +19,20 @@ void PartyHat::Display() {
 			glRotatef(rotation[2], 0.f, 0.f, 1.f);
 
 			glEnable(GL_TEXTURE_2D);
-			glEnable(GL_COLOR_MATERIAL);
-			glBindTexture(GL_TEXTURE_2D, partyHatTex);
-			glColor4f(1, 1, 1, 1);
-			GLUquadric* hat = gluNewQuadric();
-			gluQuadricNormals(hat, GLU_SMOOTH);
-			gluQuadricTexture(hat, GL_TRUE);
-			glRotatef(-90, 1, 0, 0);
-			gluCylinder(hat, 0.5f, 0, 2, 20, 20);
-			glCullFace(GL_FRONT);
-			gluCylinder(hat, 0.5f, 0, 2, 20, 20);
-			glCullFace(GL_BACK);
-			glBindTexture(GL_TEXTURE_2D, NULL);
-			glDisable(GL_COLOR_MATERIAL);
+				glEnable(GL_COLOR_MATERIAL);
+					glBindTexture(GL_TEXTURE_2D, partyHatTex);
+						glColor4f(1, 1, 1, 1); //Set the colour to opaque white
+						GLUquadric* hat = gluNewQuadric();
+						gluQuadricNormals(hat, GLU_SMOOTH);
+						gluQuadricTexture(hat, GL_TRUE);
+						glRotatef(-90, 1, 0, 0); //Rotate 90 degrees clockwise along the x axis
+						gluCylinder(hat, 0.5f, 0, 2, 20, 20); //Draw the outside of the cone (with texture)
+						glCullFace(GL_FRONT);
+						gluCylinder(hat, 0.5f, 0, 2, 20, 20); //Draw the inside of the cone (with texture)
+						glCullFace(GL_BACK);
+					glBindTexture(GL_TEXTURE_2D, NULL);
+				glDisable(GL_COLOR_MATERIAL);
 			glDisable(GL_TEXTURE_2D);
-
 		glPopAttrib();
 	glPopMatrix();
 }
