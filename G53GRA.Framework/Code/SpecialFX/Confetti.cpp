9269305c2dd9d66e2f803c516daf860d12bbe75f
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2018-04-27 07:55:16 +0100

    Refactored and commented Confetti

--- G53GRA.Framework/Code/SpecialFX/Confetti.cpp
+++ G53GRA.Framework/Code/SpecialFX/Confetti.cpp
@@ -12,18 +12,16 @@ Draws the object
 */
 void Confetti::Display() {
 	glPushMatrix();
-	glPushAttrib(GL_ALL_ATTRIB_BITS);
+		glPushAttrib(GL_ALL_ATTRIB_BITS);
+			glTranslatef(pos[0], pos[1], pos[2]);
+			glScalef(scale[0], scale[1], scale[2]);
+			glRotatef(rotation[0], 1.f, 0.f, 0.f);
+			glRotatef(rotation[1], 0.f, 1.f, 0.f);
+			glRotatef(rotation[2], 0.f, 0.f, 1.f);
 
-	glTranslatef(pos[0], pos[1], pos[2]);
-	glScalef(scale[0], scale[1], scale[2]);
-	glRotatef(rotation[0], 1.f, 0.f, 0.f);
-	glRotatef(rotation[1], 0.f, 1.f, 0.f);
-	glRotatef(rotation[2], 0.f, 0.f, 1.f);
-
-	glColor3f(r, g, b);
-	Box::box(1, 1, 0.1f);
-
-	glPopAttrib();
+			glColor4f(r, g, b, 1); //Set the colour of the confetto
+			Box::box(1, 1, 0.1f); //Draw a 1 x 1 x 0.1 cube
+		glPopAttrib();
 	glPopMatrix();
 }
 
@@ -31,6 +29,7 @@ void Confetti::Display() {
 Updates animation angles and translation vectors for the object
 */
 void Confetti::Update(const double& dT) {
+	//The following random flutters the confetto and resets its colour and position when leaving the world
 	for(int i = 0; i < 3; i++)
 		rotation[i] += rand() % 30;
 	
